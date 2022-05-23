143c471beefd70ac6376c61223791b14e4d6b04f
Aasim Shaikh <asimshaikh1993@gmail.com>
2022-05-23 08:06:04 +0530

    basic saga setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import "./App.css";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getCatsFetch } from "./catState";
 
 function App() {
-  return <div className="App"></div>;
+  const cats = useSelector((state) => state.cats.cats);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCatsFetch());
+  }, [dispatch]);
+  console.log(cats);
+  return (
+    <div className="App">
+      <h1>Cats Spices gallery</h1>
+      <p> lots of cats</p>
+      <hr />
+      <div className="gallery">
+        {cats.map((cat) => (
+          <div key={cat.id} className="row">
+            <div className="column column-left">
+              <img
+                alt={cats.name}
+                src={cat.image.url}
+                width="200"
+                height="200"
+              />
+            </div>
+            <div className="column column-right">
+              <h2>{cat.name}</h2>
+              <h5>temperament: {cat.temperament}</h5>
+              <p>{cat.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+//RTK and Saga imports
+import { Provider } from "react-redux";
+import createSagaMiddleware from "@redux-saga/core";
+import { configureStore } from "@reduxjs/toolkit";
+import catsReducer from "./catState";
+import cataSaga from "./catSaga";
+
+const saga = createSagaMiddleware();
+const store = configureStore({
+  reducer: {
+    cats: catsReducer,
+  },
+  middleware: [saga],
+});
+
+saga.run(cataSaga);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
